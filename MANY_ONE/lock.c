75bff532f0002de28e7e51fcdda763e924e059ce
Abhay Gaikwad <gaikwadab7@gmail.com>
2022-05-20 00:32:40 +0530

    Implemented functions for initialization, locking and unlocking of mutex locks.

--- MANY_ONE/lock.c
+++ MANY_ONE/lock.c
@@ -24,3 +24,23 @@ void thread_unlock(spinlock *sl){
     sl->flag = 0;
 }
 
+void mutexlock_init(mutexlock *lock){
+    lock->flag = 0;
+}
+
+void thread_mutex_lock(mutexlock *cm){
+    thread_lock(&(cm->sl));
+    while (cm->flag){
+        thread_unlock(&(cm->sl));
+        sleep(1);
+        thread_lock(&(cm->sl));
+    }
+    cm->flag = 1;
+    thread_unlock(&(cm->sl));
+}
+
+void thread_mutex_unlock(mutexlock *cm){
+    thread_lock(&(cm->sl));
+    cm->flag = 0;
+    thread_unlock(&(cm->sl));
+}
